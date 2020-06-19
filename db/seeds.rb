a19cf79f7ec95009c5d5e53dd487e6861989d55b
leinaly <v.rednay@list.ru>
2020-06-19 15:02:11 +0300

    Fix according to review

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,37 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-categories = Category.create([{title: 'Frontend'},{title: 'Backend'},{title: 'Movie Development'}])
-users      = User.create([{name: "User1", password: "pass1", type: ""}, {name: "User2", password: "pass2", type: ""}])
-tests      = Test.create([{title: "HTML", level: 1, category_id: 3}, {title: "Go", level: 2, category_id: 1}, {title: "Ruby", level: 2, category_id: 2},
-                          {title: "Java", level: 2, category_id: 2}, {title: "JS", level: 3, category_id: 1}, {title: "Python", level: 1, category_id: 2}]) questions  = Question.create([{body: "Test ruby question 1", test_id: 3}, {body: "Test ruby question 2", test_id: 3},
-                              {body: "Test html question 1",test_id: 1}, {body: "Test go question 1",test_id: 2}, {body: "Test go question 2",test_id: 2}])
-answers    = Answer.create([{body: "Test ruby answer 1", question_id: 1, correct: false}, {body: "Test ruby answer 2",  question_id: 1, correct: true},
-                            {body: "Test html answer 1",  question_id: 3, correct: true}, {body: "Test htnl answer 2", question_id: 3, correct: false},
-                            {body: "Test go answer 1", question_id: 4, correct: false}, {body: "Test go answer 2", question_id: 4, correct: true}])
-tests_users = TestsUser.create([{user_id: 1, test_id: 1}, {user_id: 2, test_id: 2}])
+cat1 = Category.create(title: 'Frontend')
+cat2 = Category.create(title: 'Backend')
+cat3 = Category.create(title: 'Movie Development')
+
+us1 = User.create(name: "User1", password: "pass1", type: "")
+us2 = User.create(name: "User2", password: "pass2", type: "")
+
+t1 = Test.create(title: "HTML", level: 1, category_id: cat3.id)
+t2 = Test.create(title: "Go", level: 2, category_id: cat1.id)
+t3 = Test.create(title: "Ruby", level: 2, category_id: cat2.id)
+t4 = Test.create(title: "Java", level: 2, category_id: cat2.id)
+t5 = Test.create(title: "JS", level: 3, category_id: cat1.id)
+t6 = Test.create(title: "Python", level: 1, category_id: cat2.id)
+
+q1 = Question.create(body: "Test ruby question 1", test_id: t3.id)
+q2 = Question.create(body: "Test ruby question 2", test_id: t3.id)
+q3 = Question.create(body: "Test html question 1",test_id: t1.id)
+q4 = Question.create(body: "Test go question 1",test_id: t2.id)
+q5 = Question.create(body: "Test go question 2",test_id: t2.id)
+
+an1 = Answer.create(body: "Test ruby answer 1", question_id: q1.id, correct: false)
+an2 = Answer.create(body: "Test ruby answer 2",  question_id: q1.id, correct: true)
+an3 = Answer.create(body: "Test html answer 1",  question_id: q3.id, correct: true)
+an4 = Answer.create(body: "Test htnl answer 2", question_id: q3.id, correct: false)
+an5 = Answer.create(body: "Test go answer 1", question_id: q4.id, correct: false)
+an6 = Answer.create(body: "Test go answer 2", question_id: q4.id, correct: true)
+
+t_u1 = TestsUser.create(user_id: us1.id, test_id: t1.id)
+t_u2 = TestsUser.create(user_id: us2.id, test_id: t2.id)
+
+
+
+
 

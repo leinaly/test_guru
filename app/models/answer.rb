8580b42e4ba26941ea3ceece7caefafadc37b311
leinaly <v.rednay@list.ru>
2020-07-05 19:52:41 +0300

    Part_6 implementation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,15 @@
 class Answer < ApplicationRecord
   belongs_to :question
+
+  scope :correct, -> { where(correct: true) }
+
+  validates :body, presence: true
+
+  validate :validate_answers_amount
+
+  private
+
+  def validate_answers_amount
+    errors.add(:question) if self.question.answers.count >= 4
+  end
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -4,8 +4,17 @@ class Test < ApplicationRecord
   has_and_belongs_to_many :users
   belongs_to :author, class_name: 'User'
 
-  def self.with_category(category_name)
-    Test.joins('JOIN categories ON categories.id = tests.category_id')
-        .where('categories.title = ?', category_name).order(title: :desc).pluck(:title)
-  end
+  scope :easy, -> { where(level: 0..1) }
+  scope :medium, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: 5..Float::INFINITY) }
+
+  scope :with_category, -> (category_name) {
+    joins(:category).where(categories: { title: category_name }).order(title: :desc).pluck(:title)
+  }
+
+  validates :title, presence: true
+  validates :level, presence: true,
+                    numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  validates_uniqueness_of :title, scope: :level
 end
